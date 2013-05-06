1ebb2efd489bf43e5f90e6095b0e1b11ebc3f9db
Heber Bastidas <heberbastidas@gmail.com>
2013-05-06 15:55:54 -0430

    update help

--- src/templer/buildout/plone_buildout.py
+++ src/templer/buildout/plone_buildout.py
@@ -4,7 +4,7 @@ class PloneBuildout(BaseTemplate):
     _template_dir = 'templates/plone_buildout'
     summary = "A basic buildout skeleton for plone 4.3"
     help = """
-This creates a basic skeleton for a buildout.
+This generate a basic buildout skeleton for plone 4.3
 """
     category = "Buildout"
     required_templates = []
